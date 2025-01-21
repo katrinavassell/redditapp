328faf17ce6f3e4f638d31df0dca06e667d9f59e
katrinavassell <38726412-katrinavassell@users.noreply.replit.com>
2025-01-21 21:35:32 +0000

    User checkpoint: Update recommended topics list.

--- src/recommendedTopics.js
+++ src/recommendedTopics.js
@@ -1,15 +1,15 @@
 
 const recommendedTopics = [
-  "indie games",
-  "indie music",
-  "indie films",
-  "indie art",
-  "indie comics",
-  "indie publishing",
-  "indie development",
-  "indie creators",
-  "indie fashion",
-  "indie culture"
+  "Finance",
+  "Work and Productivity",
+  "Engineering & Development",
+  "Design",
+  "Marketing & Sales",
+  "AI",
+  "Health & Fitness",
+  "Travel",
+  "Platforms",
+  "Ecommerce"
 ];
 
 export default recommendedTopics;
